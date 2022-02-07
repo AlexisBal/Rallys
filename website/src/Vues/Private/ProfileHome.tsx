39aa742ad348a96b1050eae8a6e417fe47999fc1
mcc <60859013+AlexisBal@users.noreply.github.com>
2022-02-07 17:14:05 +0100

    master

--- website/src/Vues/Private/ProfileHome.tsx
+++ website/src/Vues/Private/ProfileHome.tsx
@@ -10,12 +10,14 @@ import React from 'react';
 function ProfileHome () {
   let auth = useAuth();
   const [selectedFile, setSelectedFile] = useState();
+  const [hash, setHash] = useState("");
+  const [json, setJson] = useState({});
 
   const addFile = (event: any): void => {
     setSelectedFile(event.target.files[0]);
   }; 
 
-  // Publication du fichier sur IPFS
+  // Publication du fichier sur IPFS 
   const handleSubmission = async () => {
     if (selectedFile) {
       const projectId = process.env.REACT_APP_PROJECT_ID;
@@ -30,16 +32,37 @@ function ProfileHome () {
         }
       })
       const added = await node.add(selectedFile);
+      setHash(added.path);
       const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-      console.log(url)
+      console.log(url);
     } 
 	};
 
   // Récupération du fichier
   const handleGetFile = async () => {
-    const response = await fetch('https://ipfs.infura.io/ipfs/QmUGqyPXfijRyaatceGtJUFkbZuhsUAXfyjUyi1qXrBBjU');
+    const response = await fetch(`https://ipfs.infura.io/ipfs/${hash}`);
     const data = await response.json();
-    console.log(data)
+    setJson(data);
+    console.log(data);
+    console.log(json["0"]);
+  }
+
+  // Unpin a file 
+  const handleUnpinFile = async () => {
+    const projectId = process.env.REACT_APP_PROJECT_ID;
+    const projectSecret = process.env.REACT_APP_PROJECT_SECRET;
+    const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+    const node = create({
+      host: 'ipfs.infura.io',
+      port: 5001,
+      protocol: 'https',
+      headers: {
+        authorization: auth
+      }
+    })
+    await node.pin.rm(hash);
+    setHash("");
+    console.log("Fichier supprimé du répertoire")
   }
 
   return (
@@ -53,7 +76,8 @@ function ProfileHome () {
           </Form.Group>
         </Form>
         <Button variant="primary" onClick={handleSubmission}>Upload</Button>{' '}
-        <Button variant="primary" onClick={handleGetFile}>Fetch</Button>
+        <Button variant="primary" onClick={handleGetFile}>Fetch</Button>{' '}
+        <Button  variant="primary" onClick={handleUnpinFile}>Unpin</Button>
       </div>
     </div>
   );
