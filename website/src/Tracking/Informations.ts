064d05b058c9c1e55017db5be2099665fbe74db3
mcc <60859013+AlexisBal@users.noreply.github.com>
2022-03-09 12:00:25 +0100

    master

--- website/src/Tracking/Informations.ts
+++ website/src/Tracking/Informations.ts
@@ -1,26 +1,41 @@
 import { useState } from 'react';
 
 export default function Informations() {
-  const getInformations = () => {
+  const getKey = () => {
     var key = sessionStorage.getItem('key');
     if (key) {
       const userKey = JSON.parse(key);
       return userKey
-    } else {
-      return false;
-    }
-    
+    } 
+    return false;
   };
 
-  const [key, setKey] = useState(getInformations());
+  const getRallye = () => {
+    var rallyeCreation = sessionStorage.getItem('rallye-creation');
+    if (rallyeCreation) {
+      const userRallyeCreation = JSON.parse(rallyeCreation);
+      return userRallyeCreation
+    }
+    return false;
+  };
+
+  const [key, setKey] = useState(getKey());
+  const [rallye, setRallye] = useState(getRallye());
 
   const saveSessionInformations = (userKey: { key: false | any; }) => {
     sessionStorage.setItem('key', JSON.stringify(userKey.key));
     setKey(userKey.key);
   };
 
+  const saveLocalInformations = (userRallye: any) => {
+    localStorage.setItem('rallye-creation', JSON.stringify(userRallye));
+    setRallye(userRallye)
+  };
+
   return {
     setSessionInformations: saveSessionInformations,
+    setLocalInformations: saveLocalInformations,
+    rallye,
     key
-  };
+  }
 }
